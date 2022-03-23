898faf4b0e242ae525fb0ade3a66cd007513751f
D4reD3VIL81 <ahmad.81.sadeghi@gmail.com>
2022-03-23 14:21:48 +0430

    commit 'main' -m "Auth - Login completed."

--- StoreandBlog/account/urls.py
+++ StoreandBlog/account/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from views import *
+from django.urls import path,include
+from .views import *
 
 urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='account-dashboard')
+    path('dashboard/', DashboardView.as_view(), name='account-dashboard'),
+    path('',include("django.contrib.auth.urls"))
 ]
