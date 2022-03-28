87d66c1ffe9b49f4b10612c2a4cca57b7ca70c20
D4reD3VIL81 <ahmad.81.sadeghi@gmail.com>
2022-03-28 13:43:14 +0430

    commit 'main' -m 'blog - models.py finished.'

--- StoreandBlog/blog/models.py
+++ StoreandBlog/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import CharField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    text_01 = models.CharField(max_length=256)
+    text_02 = models.CharField(max_length=256)
+    text_03 = models.TextField()
+    title = models.CharField(max_length=256)
+    qouted_text_01 = models.Field(max_length=256)
+    qouted_text_02 = models.CharField(max_length=256)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
