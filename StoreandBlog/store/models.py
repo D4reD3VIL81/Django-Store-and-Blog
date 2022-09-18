c728c3e2798c8418944ab4be6b9ed4b4b53c85fb
D4reD3VIL81 <ahmad.81.sadeghi@gmail.com>
2022-09-18 12:53:38 +0430

    fgh

--- StoreandBlog/store/models.py
+++ StoreandBlog/store/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     count = models.IntegerField()
     price = models.IntegerField()
     color = models.CharField(max_length=256)
+    ziba = models.CharField(max_length=40)
 
     def get_absolute_url(self):
         return reverse("post:detail", kwargs={"slug": self.name})
